7dd6dfd96d1076b41a18fe3aa1538109f55282d0
JanetHuang <m934020033@gmail.com>
2021-01-24 23:42:51 -0800

    update

--- project1/script.js
+++ project1/script.js
@@ -1,10 +1,21 @@
 const form = document.querySelector('#task-form')
 const input = document.querySelector('input#task')
 const addBtn = document.querySelector('.btn')
-const cleanrBtn = document.querySelector('.black-btn')
+const cleanBtn = document.querySelector('.black-btn')
 const ul = document.querySelector('ul') 
+const filter = document.querySelector('#filter')
 
+//LocalStorage so far not use
+// AddLocalStorage()
+
+//Add Items
 form.addEventListener('submit', addItems)
+//Remoce items from icon
+ul.addEventListener('click', removeLi)
+//Removes items from clean button
+cleanBtn.addEventListener('click',cleanItems)
+//Filter data
+filter.addEventListener('keyup', filterText)
 
 function addItems(e){
     if (input.value ===''){
@@ -21,9 +32,75 @@ function addItems(e){
         link.innerHTML = '<i class="fas fa-times"></i>'
         newLi.appendChild(link)
         ul.appendChild(newLi)
-        console.log(newLi)
+        // console.log(newLi)    
+
+        //empty the input
         input.value = ''
+
     }
-    
     e.preventDefault();
+    
+}
+
+function AddLocalStorage(){
+    let files;
+    if (localStorage.getItem('task')===null){
+        files = [];
+        console.log('nothing')
+    }else{
+        // files = localStorage.getItem('task')
+        files = JSON.parse(localStorage.getItem('task'))
+        console.log(files, typeof(files))
+    
+        //create li & Textnode
+        let newLi = document.createElement('li');
+        let textNode = document.createTextNode(files);
+        newLi.className = 'collection-item'
+        newLi.appendChild(textNode);
+        
+        //link & icon
+        let link = document.createElement('a');
+        link.innerHTML = '<i class="fas fa-times"></i>'
+        newLi.appendChild(link)
+        ul.appendChild(newLi)
+    
+    }
+    // e.preventDefault();
+}
+
+
+
+function removeLi(e){
+    // console.log(e.target.parentElement.parentElement.classList)
+    if (e.target.parentElement.parentElement.classList.contains('collection-item')){
+        // console.log(e.target,e.target.parentElement.parentElement)
+        e.target.parentElement.parentElement.remove();
+    }
+    e.preventDefault();
+}
+
+
+function cleanItems(e){
+    if (confirm("Are you sure?")){
+        let liList = document.querySelectorAll('li')
+        liList.forEach((lis)=>{lis.remove()})
+    }
+    e.preventDefault();
+}
+
+
+function filterText(e){
+    let Text = e.target.value.toLowerCase();
+    liList = document.querySelectorAll('li')
+    liList.forEach((lis)=>{
+        let liText = lis.innerText.toLowerCase();
+        if (liText.includes(Text)){
+            lis.style.display = 'block'
+        }
+        else{
+            lis.style.display = 'none'
+        }
+    })
+
+    e.preventDefault()
 }
--- project1/test.js
+++ project1/test.js
@@ -1,9 +1,54 @@
+const form = document.querySelector('form')
 const btn = document.querySelector('.btn')
 const container = document.querySelector('.container')
 const text = document.querySelector('p.text')
 const input = document.querySelector('#input')
 const output = document.querySelector('#output')
 const select = document.querySelector('#select')
+const ul = document.querySelector('ul')
+const li = document.querySelector('li:first-child')
+
+
+//Event Bubling
+li.addEventListener('click',liClick)
+
+function liClick(e){
+    console.log('presss li')
+
+    e.preventDefault()
+}
+ul.addEventListener('click',ulClick)
+
+function ulClick(e){
+    console.log('presss ul')
+
+    e.preventDefault()
+}
+container.addEventListener('click',containerClick)
+
+function containerClick(e){
+    if(e.target.parentElement.classList.contains('collection-item')){
+        console.log('delete')
+    }
+    
+    e.preventDefault()
+}
+form.addEventListener('click',formClick)
+
+function formClick(e){
+    console.log('presss form')
+    
+    e.preventDefault()
+}
+
+// ul.addEventListener('click', btnClick)
+// function btnClick(e){
+
+//     if (ul.children)
+
+//     e.preventDefault()
+// }
+
 btn.addEventListener('click',button)
 
 function button(e){
@@ -32,7 +77,7 @@ function change(e){
 
 //LocalStorage
 
-const form = document.querySelector('form')
+//const form = document.querySelector('form')
 
 form.addEventListener('submit',action)
 
