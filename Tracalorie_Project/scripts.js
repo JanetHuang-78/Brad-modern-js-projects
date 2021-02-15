15a901737b1889088d405e8334b379bb428a403b
JanetHuang <m934020033@gmail.com>
2021-02-15 13:42:47 -0800

    update

--- Tracalorie_Project/scripts.js
+++ Tracalorie_Project/scripts.js
@@ -1,25 +1,144 @@
 //create ItemCtrl Module
 
 const itemCtrl = (function(){
-
+//Private
 const data = [
-    {id:0, name:'Cookie', cal:350},
-    {id:1, name:'Noodles', cal:550},
-    {id:2, name:'French Fries', cal:450}
-]
+    // {id:0, name:'Cookie', cal:350},
+    // {id:1, name:'Noodles', cal:550},
+    // {id:2, name:'French Fries', cal:450}
+];
+const currData =[{ID:null,name:null,cal:null}];
 
-const itemData = function (data){
+//Item Constructor:
+const itemData = function (id, name, cal){
     this.id = id;
     this.name = name;
     this.cal = cal;
 }
 
+//Public
 return{
-    InfoData: function(){
-        console.log(itemData)
+    getData: function(){
+        return data;
+    },
+    addItemData: function(name, cal){
+        let ID;
+
+        if (data.length > 0){
+            ID = data.length;
+        }else{
+            ID = 0;
+        }
+
+        //convert calories to number
+        cal = parseInt(cal);
+
+        newItem = new itemData(ID, name, cal);
+        data.push(newItem);
+        
+        return newItem;
+    },
+    getTotalCalories: function(){
+        
+        let totalCal = 0;
+        data.forEach(function(item){
+            totalCal +=item.cal
+        })
+        
+        return totalCal
+    },
+    logData: function(){
+        return data;
     }
 }
 
 
-
 })();
+
+
+const UICtrl = (function(){
+
+//Public   
+    return {
+        
+        //print out the data on browser
+        printOutList: function(data){
+            let output = ''
+            data.forEach(e => {
+                output +=`
+                <li><strong>${e.name}: </strong><span>${e.cal}</span><a href="#"><i class="fa fa-edit"></a></i></li>
+                `                
+            });
+            document.querySelector('.display').innerHTML = output
+        },
+        //grab data from UI
+        getInput: function(){
+            return {
+                name: document.querySelector('#meal').value,
+                cal : document.querySelector('#cal').value
+            }   
+        },
+        addNewItem: function(data){
+            const li = document.createElement('li');
+            li.id = `item-${data.ID}`
+            li.innerHTML = `
+            <strong>${data.name}: </strong><span>${data.cal}</span><a href="#"><i class="fa fa-edit"></a></i>
+            `
+            //Before the end of the element (as the last child)
+            document.querySelector('.display').insertAdjacentElement('beforeend',li)
+        },
+        updatTotalCal: function(totalCal){
+            
+            document.querySelector('span.showCal').textContent = totalCal;
+        },
+        clearInput: function(){
+            document.querySelector('#meal').value = '';
+            document.querySelector('#cal').value = '';
+        }
+    }
+})()
+
+
+//main control
+const app = (function(itemCtrl, UICtrl){
+
+    //Load EventListener
+    const loadEventListener = function(){
+        //when add-button is pressed
+        const addBtn = document.querySelector('.add');
+        addBtn.addEventListener('click', ItemAddSubmit)
+    }
+
+    //Add Item Submit
+    function ItemAddSubmit(e){
+        //get form input
+        const input = UICtrl.getInput();
+        if (input.name!=='' && input.cal!==''){
+            const newData = itemCtrl.addItemData(input.name, input.cal)
+            UICtrl.addNewItem(newData);
+            UICtrl.clearInput();
+            //get total Calories
+            const totalCal = itemCtrl.getTotalCalories();
+            //Update total cal 
+            UICtrl.updatTotalCal(totalCal);
+        }
+
+        e.preventDefault();
+    }
+
+    //Initialize
+    return{
+        init: function(){
+            console.log('Initialing...')
+            // const dataList = itemCtrl.getData();
+            // UICtrl.printOutList(dataList);
+            //call loadEventListener in order to prepare activating ItemAddSubmit
+            loadEventListener(); 
+            
+            
+        }
+    }
+})(itemCtrl, UICtrl)
+
+//Initialize everything in the app 
+app.init();
